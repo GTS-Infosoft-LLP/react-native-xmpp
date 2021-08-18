e057763e649bef7b738e2a1eea1ac111575ae83a
Zyauddin <71919615+Zyauddin@users.noreply.github.com>
2021-08-18 14:18:53 +0530

    react module issue fixed

--- RNXMPP/RNXMPP.h
+++ RNXMPP/RNXMPP.h
@@ -7,10 +7,30 @@
 //
 
 #import <Foundation/Foundation.h>
+#if __has_include("RCTBridgeModule.h")
+#import "RCTBridgeModule.h"
+#else
 #import <React/RCTBridgeModule.h>
+#endif
+
+#if __has_include("RCTBridge.h")
+#import "RCTBridge.h"
+#else
 #import <React/RCTBridge.h>
+#endif
+
+#if __has_include("RCTEventDispatcher.h")
+#import "RCTEventDispatcher.h"
+#else
 #import <React/RCTEventDispatcher.h>
+#endif
+
+#if __has_include("RCTConvert.h")
+#import "RCTConvert.h"
+#else
 #import <React/RCTConvert.h>
+#endif
+
 #import "RNXMPPService.h"
 
 @interface RNXMPP : NSObject<RCTBridgeModule, RNXMPPServiceDelegate>
